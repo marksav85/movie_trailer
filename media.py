3987836fb3d77ac24a6e333223163cfb129f50fa
marksav85 <mark.saville@outlook.com>
2020-12-17 21:16:28 +0100

    Update a second movie trailer

--- media.py
+++ media.py
@@ -20,11 +20,11 @@ print(spongebob.storyline)
 
 spongebob.show_trailer()
 
-avatar = media.Movie("Avatar","A marine on an alien planet","https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg","https://www.youtube.com/watch?v=5PSNL1qE6VY&ab_channel=20thCenturyStudios")
+guardians_of_the_galaxy = media.Movie("Guardians of the Galaxy","A group of misfits band together to defend the galaxy","https://upload.wikimedia.org/wikipedia/en/b/b5/Guardians_of_the_Galaxy_poster.jpg","https://www.youtube.com/watch?v=d96cjJhvlMA&ab_channel=MarvelEntertainment")
 
-print(avatar.storyline)
+print(guardians_of_the_galaxy.storyline)
 
-avatar.show_trailer()
+guardians_of_the_galaxy.show_trailer()
 
 dredd = media.Movie("Dredd","A lone enforcer takes on a drug gang in a dystopian future","https://upload.wikimedia.org/wikipedia/en/1/16/Dredd2012Poster.jpg","https://www.youtube.com/watch?v=qVIba2N6MTA")
 
@@ -32,7 +32,7 @@ print(dredd.title, dredd.storyline)
 
 dredd.show_trailer()
 
-movies = [spongebob, avatar, dredd]
+movies = [spongebob, guardians_of_the_galaxy, dredd]
 
 fresh_tomatoes.open_movies_page(movies)
 
